341d4e8c6d2e6523b866c9ab1d7579fed65cab8b
tatsuya-asami <asami.tatsuya@commmune.jp>
2024-07-07 18:30:54 +0900

    refactor: Import CONSTANT1 in feature1 and utils1
    This commit refactors the code to import the CONSTANT1 from the constants1 module in both the feature1 and utils1 modules. This improves code organization and reduces duplication.

--- src/features/features1/index.ts
+++ src/features/features1/index.ts
@@ -1,1 +1,7 @@
+import { CONSTANT1 } from "../../constants/constants1";
+import { UTIL1 } from "../../utils/utils1";
+
 export const FEATURE1 = "FEATURE1";
+
+console.log(CONSTANT1);
+console.log(UTIL1);
--- src/utils/utils1/index.ts
+++ src/utils/utils1/index.ts
@@ -1,1 +1,5 @@
+import { CONSTANT1 } from "../../constants/constants1";
+
 export const UTIL1 = "UTIL1";
+
+console.log(CONSTANT1);
